fbce07d8f90ad3fcb9503c7e1aab2b75cc319014
Shamsu <babaas@amazon.com>
2024-01-13 12:24:52 -0500

    creating variables of constant object bookbag

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,37 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bookbag = {
+    name: "School bag",
+    volume: 50,
+    color: "blue",
+    pocketNum: 10,
+    strapLength: {
+        left: 30,
+        right: 30,
+    }, 
+    coverOpen: false,
+    pencilPocketLid: function (lidStatus) {
+        this.coverOpen = lidStatus;     
+    },
+    
+    newStrapLength: function (lengthLeft, lengthRight) {
+        if(typeof lengthLeft !== 'number' || typeof lengthRight !== 'number'){
+            return "Error! Both arguments must be numbers.";
+            } else {
+                this.strapLength.left = lengthLeft; 
+                this.strapLength.right = lengthRight;
+                }
+    }
+    };
+
+
+
+
+
+
+    console.log("My bookbag:", bookbag); // Accessing the whole object
+    console.log("The Pencil Pocket Lid:", bookbag.pocketNum);
+    
+
