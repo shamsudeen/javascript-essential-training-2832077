146b93213bb40cd99d83e0f627ad23df4b76432b
Shamsu <babaas@amazon.com>
2024-01-18 10:57:27 -0500

    added codes

--- 04_02/script.js
+++ 04_02/script.js
@@ -3,6 +3,7 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  *
  */
+
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
@@ -38,3 +39,37 @@ document.body.innerHTML = content;
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
+
+
+// eslint-disable-next-line no-irregular-whitespace
+// // JavaScript code​​​​​​‌​‌​‌​‌​​​​‌‌‌‌​​​​‌​​​​ below
+// Write your answer here, then test your code.
+// Your job is to implement the findLargest() method.
+
+// Change these boolean values to control whether you see 
+// the expected answer and/or hints.
+// const showExpectedResult = false;
+// const showHints = false;
+
+// function Camera(brand, model, year, format, lens, filmType) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.year = year;
+//     this.format = format;
+//     this.lens = lens;
+//     this.filmType = filmType;
+// }
+
+// const getCurrentYear = () => new Date().getFullYear();
+
+// const cameraAge = (year) => getCurrentYear() - year;
+
+// // Goal output:
+// // My camera is a [brand] [model] made in [year] making it [age] years old. It's a [format] camera with a [lens] lens using [filmtype] film.`
+// const cameraHTML = (myCamera) => {
+//     // Your code goes between the backticks in `cameraStory` below.
+//     const cameraStory = `My camera is a ${myCamera.brand} ${myCamera.model} made in ${myCamera.year} making it ${cameraAge(myCamera.year)} years old. It's a ${myCamera.format} camera with a ${myCamera.lens} lens using ${myCamera.filmType} film.`;
+//     return cameraStory;
+// }    
+
+
