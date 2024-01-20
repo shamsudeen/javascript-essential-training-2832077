859f0f8517b41f9a8fcbab78272814e47551204d
Shamsu <babaas@amazon.com>
2024-01-20 14:24:10 -0500

    changes

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,8 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+ // Initialize a variable with the value of 42
+ let answer = 42;
+ console.log(answer); // Output: 42
+ 
+ 
