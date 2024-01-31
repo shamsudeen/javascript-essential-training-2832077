1fc04257b566f46957a775a8736dc7044bf9c1a7
Shamsu Baba-Alwaiz <shamsubaba@gmail.com>
2024-01-31 10:26:00 -0500

    changes

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,21 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// Article Object
+const createArticle = (article) => {
+  const el = document.createElement('ARTICLE');
+  // Add title to the article using helper function
+  addTitle(el, article.title);
+  return el;
+};
+
+// Title Helper Function
+const addTitle = (parent, text) => {
+  const h1El = document.createElement('H1');
+  h1El.textContent = text;
+  parent.appendChild(h1El);
+}
+
+// Call main function and pass an example article
+console.log(createArticle({ title: 'This is a test' }));
