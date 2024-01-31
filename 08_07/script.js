055a14505bccc4398ee2f4ea5b6e9a93fd35942f
Shamsu Baba-Alwaiz <shamsubaba@gmail.com>
2024-01-31 12:28:10 -0500

    changes

--- 08_07/script.js
+++ 08_07/script.js
@@ -3,17 +3,31 @@
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
  */
 
-const tipCalculator = () => {
-  let sum = 29.95;
-  let percentage = 18;
+const tipCalculator = (sum, percentage, currency, prefix) => {
+  // let sum = 29.95;
+  // let percentage = 18;
   let tip = sum * (percentage / 100);
   let total = sum + tip;
-  console.log(`
-  Sum before tip: ${sum}
+  if (prefix){
+      console.log(`
+      Sum before tip: ${currency}${sum}
+      Tip percentage: ${currency}${percentage}%
+      Tip:            ${currency}${tip.toFixed(2)}
+      Total:          ${currency}${total.toFixed(2)}
+    `);
+    
+  } else {
+     console.log(`
+  Sum before tip: ${sum}${currency}
   Tip percentage: ${percentage}%
-  Tip:            ${tip.toFixed(2)}
-  Total:          ${total.toFixed(2)}
+  Tip:            ${tip.toFixed(2)}${currency}
+  Total:          ${total.toFixed(2)}${currency}
 `);
+    
+  }
+
 };
 
-tipCalculator();
+tipCalculator(29.95, 28,  "$", true);
+// Outputs: "Sum before tip: 29.95\nTip percentage: 28%\nTip:            7.64\nTotal:                    37.69"
+// Outputs: "Sum before tip: 29.95\nTip percentage: 28%\nTip:            7.64\nTotal:          37.69" 
