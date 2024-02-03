65c9f96e146d1deef9d7e5848ec5fbdf23e0b37a
Shamsu Baba-Alwaiz <shamsubaba@gmail.com>
2024-02-03 17:36:09 -0500

    changes

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,8 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+
+
+    
+
--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,48 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+
+// HTML elements of the grid cells
+const gridCells = document.querySelectorAll('.grid-cell');
+/**
+ * 
+ * 
+ *   @param {Event} e The event object passed by the browser
+ */
+// eslint-disable-next-line no-unused-vars
+function handleMouseOver(e) {   
+    // Highlights the current cell when mouseover occurs
+    this.classList.add('highlighted');        
+}
+
+/**
+ * Function to remove the 'highlight' class from a given element, which makes it stop being highlighted.
+ * This function is used as the callback for both `handleMouseOver` and `handleClick`.
+ *  
+ * @param {Element} elm The HTML element to be un-highlighted
+ */
+function removeHighlight(elm){
+    elm.classList.remove('highlighted')    
+}
+
+/**
+ * When a grid cell is clicked, we add the "clicked" class to it. We also call the `removeHighlight` function
+ * with the clicked element after a short delay (to show the user that the click was successful).
+ *  
+ * @param {Event} _e The event object passed by the browser
+ */
+// eslint-disable-next-line no-unused-vars
+function handleClick(e) {                            
+    this.classList.add("clicked");       
+    setTimeout(() => removeHighlight(this), 1000);            
+}   
+
+// Adding event listeners to all grid cells
+gridCells.forEach((cell)=>{
+    cell.addEventListener('mouseover', handleMouseOver);
+    cell.addEventListener('click', handleClick);
+});
+
+    
+    
